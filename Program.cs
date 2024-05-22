2ade26388c62717f2484123fb005ce8672e6d7ab
defntjames <113400320+defntjames@users.noreply.github.com>
2024-05-22 22:39:22 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 500;
+
+            x *= ((500 + 1000) / 10) - (30 * 2 ^ 2);
+
+            Console.WriteLine(x);
+        }
+    }
+}
